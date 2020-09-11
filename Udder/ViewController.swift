bca9a5eef074fd66da707ca9debd3cb2ad5aaa82
Vova Arty <1arty4u@gmail.com>
2020-09-11 10:54:55 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by AV on 2020/9/11.
+//  Copyright © 2020 Vova Arty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
